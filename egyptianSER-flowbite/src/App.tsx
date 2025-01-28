1227ac67b42440d8b7b8b3b7b71a26a9d01aa3c0
Yousef Moustafa <ymoustafa005@gmail.com>
2025-01-28 03:27:13 +0200

    Initial commit: Setup React app with Vite, Tailwind CSS, and Flowbite

---
+++ egyptianSER-flowbite/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Button } from 'flowbite-react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>My Speech Emotion Recognition Application</h1>
+    <Button>Predict Emotion</Button>
+    </>
+  )
+}
+
+export default App
