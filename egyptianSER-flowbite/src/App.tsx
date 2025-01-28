0fa46ff21ee432dd0c5fc6bbae8f8e37529559d4
Yousef Moustafa <ymoustafa005@gmail.com>
2025-01-28 04:34:32 +0200

    feat: Add core layout with file upload and prediction button

--- egyptianSER-flowbite/src/App.tsx
+++ egyptianSER-flowbite/src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import TutorialComponent from "./playground/TutorialComponent";
-import './App.css'
+import { Button } from 'flowbite-react';
+import { FileuploadComponent } from './components/FileuploadComponent';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <TutorialComponent />
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold mb-4 text-white-900">
+        My Speech Emotion Recognition Application
+      </h1>
+      <div className="w-full max-w-lg mb-6">
+        <FileuploadComponent />
+      </div>
+
+      <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded dark:bg-blue-600 dark:hover:bg-blue-800">
+        Predict Emotion
+      </Button>
     </div>
   );
 }
 
-export default App
+export default App;
